61c6d09375c6605c7fa4a84e530a81df3bc4b9e2
Dariioo1 <dariopaucar@outlook.es>
2021-07-21 20:16:07 +0200

    ConstructorGetYSet

--- punto.ts
+++ punto.ts
@@ -3,6 +3,26 @@ export class Punto
    private x:number;
    private y:number;
 
-   
+   constructor(x:number, y:number)
+   {
+       this.x=x;
+       this.y=y;
+   }
+   public getX():number
+   {
+       return this.x;
+   }
+   public getY():number
+   {
+       return this.y;
+   }
+   public setx(x:number)
+   {
+       this.x=x;
+   }
+   public setY(y:number)
+   {
+       this.y=y
+   }
     
 }
